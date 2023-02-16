11c8a954c9123de9e6863aa8fbddf98f044af5f2
crazyloongcat <crazyloongcat@163.com>
2023-02-16 20:22:47 +0800

    小程序获取手机号不允许使用 小程序服务器域名进行直接访问

--- pages/loginButtonIndex/index.js
+++ pages/loginButtonIndex/index.js
@@ -18,21 +18,8 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-    wx.request({
-      url: 'https://api.weixin.qq.com/cgi-bin/token',//文档请求地址
-      data: {
-        grant_type: "client_credential",//这个是固定的
-        appid: 'wx0bc3ed21ec0db2be',
-        secret: '5db1652877c0624e6a7a412dcd4a3373'
-      },
-      success: (res) => {
-        console.log("get", res);
-        this.setData({
-          access_token: res.data.access_token
-        })
-      }
-    })
+  async onLoad(options) {
+    
   },
 
   getPhoneNumber(e) {
@@ -46,29 +33,7 @@ Page({
     this.setData({
       code: e.detail.code
     })
-    
-    wx.request({//微信小程序官方接口 文档里面有哈
-      url: "https://api.weixin.qq.com/wxa/business/getuserphonenumber?access_token=" +  this.data.access_token, //文档请求地址 需要传送两个参数 这个参数是要拼接地址的 参数一接口调用凭证
-      method: "POST",
-      header: {
-        'content-type': 'application/json' //转换为json  参数2 需要转换json格式
-      },
-      data: {
-        code: this.data.code //参数二
-      },
-      success: (res) => {
-        console.log(res,'------');
-        this.setData({
-          phoneNumber: res.data.phone_info.phoneNumber,//这个是获取到的手机号 
-          appid: res.data.phone_info.watermark.appid //这个是获取到的appid
-        })
-        this.submitNickName()
-        console.log(this.data.phoneNumber);
-      },
-      error: (  err ) => {
-        console.log(err,'---------')
-      }
-    })
+    this.submitNickName()
   },
 
  butNo(){
@@ -79,9 +44,9 @@ Page({
 async submitNickName() {
   const res = await request({
     method: "POST",
-    url: "/webapi/ap/user/submit/nick/name",
+    url: "/webapi/ap/user/submitPhone",
     data: {
-      nickName: this.data.phoneNumber
+      code: this.data.code
     }
   });
   if (res.code == 0) {
